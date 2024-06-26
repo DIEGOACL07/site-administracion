fed0473ee164206342ef0617496604b9bebcd738
DIEGOACL07 <105613359+DIEGOACL07@users.noreply.github.com>
2024-06-26 10:02:34 -0500

    Implentando el SessionStorage

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 import { create } from "zustand";
-import { devtools } from "zustand/middleware";
+import { devtools, persist } from "zustand/middleware";
 import { DraftPerson, Person } from "./types";
 
 type PersonState = {
@@ -12,33 +12,37 @@ type PersonState = {
     updatePerson: (data: DraftPerson) => void
 }
 
-const createPerson = (person: DraftPerson) : Person => {
-    return {...person, id: uuidv4()}
+const createPerson = (person: DraftPerson): Person => {
+    return { ...person, id: uuidv4() }
 }
 
 export const usePersonStore = create<PersonState>()(
-    devtools((set) => ({
-    persons: [],
-    activeId: '',
-    addPerson: (data) => {
-        const newPerson = createPerson(data)
-        set((state) => ({
-            persons: [...state.persons, newPerson]
-        }))
-    },
-    deletePerson:(id) => {
-        set((state) => ({
-            persons: state.persons.filter(person => person.id !== id),
-        }))
-    },
-    getPatientByid:(id) => {
-        set((state) => ({
-            activeId: id,
-        }))
-    },
-    updatePerson: (data) => {
-        set((state) => ({
-            persons: state.persons.map(person => person.id == state.activeId ? {id: state.activeId, ...data}: person), activeId: "",
-        }))
-    }
-})))
+    devtools(
+        persist((set) => ({
+            persons: [],
+            activeId: '',
+            addPerson: (data) => {
+                const newPerson = createPerson(data)
+                set((state) => ({
+                    persons: [...state.persons, newPerson]
+                }))
+            },
+            deletePerson: (id) => {
+                set((state) => ({
+                    persons: state.persons.filter(person => person.id !== id),
+                }))
+            },
+            getPatientByid: (id) => {
+                set((state) => ({
+                    activeId: id,
+                }))
+            },
+            updatePerson: (data) => {
+                set((state) => ({
+                    persons: state.persons.map(person => person.id == state.activeId ? { id: state.activeId, ...data } : person), activeId: "",
+                }))
+            }
+        }), {
+            name: "person-storage",
+        }
+    )))
